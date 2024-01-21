42a24b9c891b918ec8c869b6158623374fc321d1
vineesh <vineeshw1994@gmail.com>
2024-01-21 21:13:03 +0530

    add a middleware and a funtion to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,24 @@
 import User from "../models/User.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup =async(req, res) =>{
-   const {username, email, password} = req.body;
+export const signup = async (req, res, next) => {
+   const { username, email, password } = req.body;
 
-   if(!username || !email || !password || username === '' || email === '' || password === ''){
-      return res.status(400).json({error: "Please add all the fields"});
+   if (!username || !email || !password || username === '' || email === '' || password === '') {
+      next(errorHandler(400, 'All fields are required'))
    }
- const hashedPassword =  bcrypt.hashSync(password, 10);
 
-   try{
-      const newUser = new User({username, email, password:hashedPassword});
+   try {
+
+      const hashedPassword = bcrypt.hashSync(password, 10);
+
+      const newUser = new User({ username, email, password: hashedPassword });
 
       await newUser.save();
-      res.json({message: "User registered successfully"});
-   }catch(err){
-  res.status(500).json({message: err.message});
+      res.json({ message: "User registered successfully" });
+   } catch (err) {
+      next(err)
    }
-  
+
 } 
--- api/index.js
+++ api/index.js
@@ -18,7 +18,18 @@ app.use(express.json());
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message 
+    })
+})
+ 
 
 app.listen(3000, () => {
     console.log('server is running ') 
+
 }) 
