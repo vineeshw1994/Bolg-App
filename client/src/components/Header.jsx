28b1a9db5495c8f45ad10c4d2206b5ece7f59308
vineesh <vineeshw1994@gmail.com>
2024-01-21 10:16:33 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,11 +29,13 @@ const Header = () => {
       </Button>
 
       <div className="flex gap-2 md:order-2">
+      
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
+
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
         </Link>
 
         <Navbar.Toggle />
