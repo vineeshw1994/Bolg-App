af4f44139636d52790915ca260a77cae1341912c
vineesh <vineeshw1994@gmail.com>
2024-01-24 15:14:46 +0530

    add posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "flowbite-react"
 import { useEffect, useState } from "react"
-import { HiArrowSmRight, HiUser } from "react-icons/hi"
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi"
 import { Link, useLocation } from "react-router-dom"
 import { signoutSuccess } from "../redux/user/UserSlice"
 import { useDispatch, useSelector } from "react-redux"
@@ -42,12 +42,20 @@ const DashSidebar = () => {
   return (
     <Sidebar className="w-full md:w-56" >
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to={'/dashboard?tab=profile'}>
-            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark' className='cursor-pointer' as='div'>
+            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' className='cursor-pointer' as='div'>
               Profile
             </Sidebar.Item>
           </Link>
+          
+          {currentUser.isAdmin && (
+            <Link to={'/dashboard?tab=posts'}>
+            <Sidebar.Item active={tab === 'posts'} icon={HiDocumentText}  className='cursor-pointer' as='div'>
+             Posts
+            </Sidebar.Item>
+          </Link>
+          )}
           
           <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}  >
             Sign Out
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import DashSidebar from "../components/DashSidebar"
 import DashProfile from "../components/DashProfile"
+import DashPost from "../components/DashPost"
 const Dashboard = () => {
   const location = useLocation()
   const [tab, setTab] = useState('')
@@ -22,6 +23,8 @@ const Dashboard = () => {
     </div>
     {/* profile */}
     {tab === 'profile' && <DashProfile />}
+    {/* posts */}
+    {tab === 'posts' && <DashPost/>}
     </div>
   )
 }
