fa9da3302cb150b4dd9775a7162962546a572c7f
vineesh <vineeshw1994@gmail.com>
2024-01-30 12:52:35 +0530

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,18 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div >
+          <h1 className="text-3xl font-semibold text-center my-7"> Logic Lens</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Welcome to Logic Lens, your digital hub for insightful perspectives and logical analyses. Dive into our world where we unravel complex topics, explore diverse viewpoints, and illuminate the path to understanding. Join us on this journey as we bring clarity through the lens of reason and intellect.</p>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
