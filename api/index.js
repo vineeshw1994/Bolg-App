ac93adbd7cca053042ea09bd5ca185cc646ed16a
vineesh <vineeshw1994@gmail.com>
2024-02-05 16:47:33 +0530

    update the prefix installtion path

--- api/index.js
+++ api/index.js
@@ -7,8 +7,11 @@ import postRoutes from './routes/Post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 dotenv.config();  
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 mongoose.connect(process.env.MONGO).then(()=>{ 
     console.log('mongodb is connected')
@@ -16,24 +19,24 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err)
 }) 
 
-const __dirname = path.resolve();
   
 const app = express(); 
   
 app.use(express.json());
 app.use(cookieParser()) 
- 
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes) 
 app.use('/api/post', postRoutes)
 app.use('/api/comment',commentRoutes)
 
-app.use(express.static(path.join(__dirname, 'client','dist','index.html')))
-
 app.get('*', (req, res) => {
-     res.sendFile(path.join(__dirname, '/client/dist/index.html'))
-})
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
+ 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
