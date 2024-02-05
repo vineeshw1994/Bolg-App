a0b3eac6be2ee194554027a7433066616a08a712
vineesh <vineeshw1994@gmail.com>
2024-02-05 22:19:06 +0530

    main .jsx file updated path clear

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store, persistor } from './redux/Store'
+import { store, persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import ThemeProvider from './components/ThemeProvider.jsx'
